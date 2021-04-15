06e13748d660d78ee5cb4887662479aad5d8a05a
Peter Eang <peang@mail.greenriver.edu>
2021-04-15 12:31:52 -0400

    Created index.php page

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Name: Peter Eang
+ * Date: 15/4/2021
+ * File: index.php
+*/
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Programming 2</title>
+</head>
+<body>
+    <h1>Pair Programming 2</h1>
+    <?php
+        // Step 1
+        echo "<p>PHP Array Practice</p>";
+
+    ?>
+</body>
+</html>
